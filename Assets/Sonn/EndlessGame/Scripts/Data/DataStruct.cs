83b6de511df2bf52aced113b9d7d46fc3fa6375b
Son <lvson21102k4@gmail.com>
2025-07-26 00:21:04 +0700

    Create
    DataStruct Script

---
+++ Assets/Sonn/EndlessGame/Scripts/Data/DataStruct.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sonn.EndlessGame
+{
+    public enum GameTag
+    {
+        Player,
+        Block,
+        DeadZone
+    }
+
+    public enum GameLayer
+    {
+        Player,
+        Block,
+        Dead
+    }
+
+    public enum CharacterAnimator
+    {
+        Idle,
+        Jump,
+        Land,
+        Dead
+    }
+
+    public enum GamePref
+    {
+        BestScore,
+        LevelPrefix,
+        CurrentPlayerID,
+        IsMusicOn,
+        IsSoundOn
+    }
+
+    public enum GameScene
+    {
+        MainMenu,
+        GamePlay,
+    }
+
+    public enum MoveDirection
+    {
+        Left,
+        Right
+    }
+
+    public enum GameState
+    {
+        Starting,
+        Playing,
+        GameOver
+    }
+}
