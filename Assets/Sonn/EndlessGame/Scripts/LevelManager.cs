d18d4efaaf9546542e56d2396ba98f32b3e09f4c
Son <lvson21102k4@gmail.com>
2025-07-26 13:13:32 +0700

    Level Manager Script
    Complete

--- Assets/Sonn/EndlessGame/Scripts/LevelManager.cs
+++ Assets/Sonn/EndlessGame/Scripts/LevelManager.cs
@@ -27,17 +27,33 @@ namespace Sonn.EndlessGame
                 {
                     continue;
                 } 
+                string levelDatakey = GamePref.LevelUnlocked.ToString() + i;
+
                 if (i == 0)
                 {
                     // Mở khóa level đầu tiên
+                    Pref.SetLevelUnlocked(i, true);
                 }
                 else
                 {
                     // Nếu dữ liệu chưa được lưu xuống máy người chơi thì 
                     // sẽ lưu dữ liệu (khóa các level khác lại)
+                    if (!PlayerPrefs.HasKey(levelDatakey))
+                    {
+                        Pref.SetLevelUnlocked(i, false);
+                    }
                 }    
 
             }    
+        }    
+
+        public LevelItem GetLevel()
+        {
+            if (levels != null && levels.Length > 0)
+            {
+                return levels[Pref.CurrentLevel_ID];
+            }    
+            return null;
         }    
 
         public void MakeSingleton()
